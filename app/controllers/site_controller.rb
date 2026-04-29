7aac5ce1b3883254b508d36161b6a6120753de20
Thomas Kenny <thomas@tallymarket.co.uk>
2026-04-29 21:31:32 +0100

    roof inspection mock up

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,4 +2,7 @@ class SiteController < ApplicationController
 
   def home
   end
+
+  def roof_inspection_reports
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'site#home'
+  get 'roof-inspection-reports', to: 'site#roof_inspection_reports', as: :roof_inspection_reports
 end
