f619d9331b7e7db77627b4c962ea7a8188ab62a0
Thomas Kenny <thomas@tallymarket.co.uk>
2026-09-25 10:57:01 +0100

    nav tidy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   WHATSAPP_NUMBER = '447460839910'.freeze
 
   def whatsapp_enquiry_url(message = nil)
-    text = message.presence || "Hi Scutts, I'm interested in eco / solar roof options. Postcode: "
+    text = message.presence || "Hi there, I'm interested in eco / solar roof options."
     "https://wa.me/#{WHATSAPP_NUMBER}?text=#{ERB::Util.url_encode(text)}"
   end
 end
